fc8c567bc289712b86e47abdcd19d902e1df5fd4
Jani Kaarela <jani.kaarela@gmail.com>
2015-08-28 20:27:10 +0300

    Tehtävä 53: viestien UI.

--- public/app/controllers/show_message_controller.js
+++ public/app/controllers/show_message_controller.js
@@ -3,4 +3,16 @@ FoorumApp.controller('ShowMessageController', function($scope, $routeParams, Api
   Api.getMessage(messageId).success(function(message) {
     $scope.message = message;
   });
+
+  $scope.reply;
+  $scope.addReply = function() {
+    if ($scope.reply) {
+      var reply = {
+        content: $scope.reply
+      };
+      Api.addReply(reply, messageId).success(function(reply) {
+        $scope.message.Replies.push(reply);
+      });
+    }
+  }
 });
--- public/app/controllers/show_topic_controller.js
+++ public/app/controllers/show_topic_controller.js
@@ -3,4 +3,18 @@ FoorumApp.controller('ShowTopicController', function($scope, $routeParams, $loca
   Api.getTopic(topicId).success(function(topic) {
     $scope.topic = topic;
   });
+
+  $scope.messageTitle;
+  $scope.messageContent;
+  $scope.addMessage = function() {
+    if ($scope.messageTitle && $scope.messageTitle) {
+      var message = {
+        title: $scope.messageTitle,
+        content: $scope.messageContent
+      };
+      Api.addMessage(message, topicId).success(function(message) {
+        $location.path('/messages/' + message.id);
+      });
+    }
+  };
 });
